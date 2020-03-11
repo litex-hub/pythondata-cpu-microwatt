2a025c3bcd92915ce108d1dc5481615f6620c71c
LiteX <robot@timvideos.us>
2020-03-11 05:39:40 +0000

    Updating data to v0.0-415-gcc20989
    Updated to 0.0.post415 based on cc20989bdab42702be37b547c74bfa55962df34d from https://github.com/antonblanchard/microwatt.
    > commit cc20989bdab42702be37b547c74bfa55962df34d
    > Merge: 44f53f6 54a399f
    > Author: Anton Blanchard <anton@linux.ibm.com>
    > Date:   Wed Mar 11 12:46:01 2020 +1100
    >
    >     Merge pull request #152 from mikey/gitignore
    >
    >     gitignore: Fix RAM file name
    >
    Updated using v0.0-28-g2e69c37 from https://github.com/litex-hub/litex-data-auto

--- litex/data/cpu/microwatt/__init__.py
+++ litex/data/cpu/microwatt/__init__.py
@@ -2,12 +2,12 @@ import os.path
 __dir__ = os.path.split(os.path.abspath(os.path.realpath(__file__)))[0]
 data_location = os.path.join(__dir__, "vhdl")
 src = "https://github.com/antonblanchard/microwatt"
-git_hash = "44f53f6bdafe85abc4e7ea783b4169629ed7f686"
-git_describe = "v0.0-413-g44f53f6"
-version_str = "0.0.post413"
+git_hash = "cc20989bdab42702be37b547c74bfa55962df34d"
+git_describe = "v0.0-415-gcc20989"
+version_str = "0.0.post415"
 version_tuple = (0, 0)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post413")
+    pversion = V("0.0.post415")
 except ImportError:
     pass
