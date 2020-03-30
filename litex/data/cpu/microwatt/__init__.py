1e99ce250598e070ab9542fb230d120b2f92d1ec
LiteX <robot@timvideos.us>
2020-03-30 05:49:10 +0000

    Updating data to v0.0-426-g8bee4ae
    Updated to 0.0.post426 based on 8bee4ae3cc629098b13a36a11c97284fc0509bed from https://github.com/antonblanchard/microwatt.
    > commit 8bee4ae3cc629098b13a36a11c97284fc0509bed
    > Merge: cc20989 8136918
    > Author: Anton Blanchard <anton@linux.ibm.com>
    > Date:   Mon Mar 30 13:46:23 2020 +1100
    >
    >     Merge pull request #153 from paulusmack/master
    >
    >     Load/store improvements
    >
    Updated using v0.0-28-g2e69c37 from https://github.com/litex-hub/litex-data-auto

--- litex/data/cpu/microwatt/__init__.py
+++ litex/data/cpu/microwatt/__init__.py
@@ -2,12 +2,12 @@ import os.path
 __dir__ = os.path.split(os.path.abspath(os.path.realpath(__file__)))[0]
 data_location = os.path.join(__dir__, "vhdl")
 src = "https://github.com/antonblanchard/microwatt"
-git_hash = "cc20989bdab42702be37b547c74bfa55962df34d"
-git_describe = "v0.0-415-gcc20989"
-version_str = "0.0.post415"
+git_hash = "8bee4ae3cc629098b13a36a11c97284fc0509bed"
+git_describe = "v0.0-426-g8bee4ae"
+version_str = "0.0.post426"
 version_tuple = (0, 0)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post415")
+    pversion = V("0.0.post426")
 except ImportError:
     pass
