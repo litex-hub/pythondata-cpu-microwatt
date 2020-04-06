e52f2c1808eebf494246a1796a6d8d0b5b6b75c7
Tim 'mithro' Ansell <me@mith.ro>
2020-04-05 21:37:57 -0700

    Updating data to v0.0-431-gd3291db
    Updated to 0.0.post431 based on d3291dbdf01960e900e1d863f02f35a45a611454 from https://github.com/antonblanchard/microwatt.
    > commit d3291dbdf01960e900e1d863f02f35a45a611454
    > Merge: 8bee4ae 9d7df2d
    > Author: Anton Blanchard <anton@linux.ibm.com>
    > Date:   Fri Apr 3 10:36:36 2020 +1100
    >
    >     Merge pull request #155 from mikey/exceptions
    >
    >     Add decrementer, illegal and system call exceptions
    >
    Updated using v0.0-41-g7ff03a7 from https://github.com/litex-hub/litex-data-auto

--- test.py
+++ test.py
@@ -6,5 +6,9 @@ from litex.data.cpu import microwatt
 print("Found microwatt @ version", microwatt.version_str, "("+microwatt.git_hash+")")
 print("Data is in", microwatt.data_location)
 assert os.path.exists(microwatt.data_location)
-print("It contains:\n -", end=" ")
-print("\n - ".join(os.listdir(microwatt.data_location)))
+print("It contains:")
+for root, dirs, files in os.walk(microwatt.data_location):
+    dirs.sort()
+    for f in sorted(files):
+        path = os.path.relpath(os.path.join(root, f), microwatt.data_location)
+        print(" -", path)
