fe27d59c9aa548928474b2ebc4a4c62fabb15144
LiteX <robot@timvideos.us>
2020-04-06 05:52:52 +0000

    Updating data to v0.0-431-gd3291db
    Updated to 0.0.post431 based on d3291dbdf01960e900e1d863f02f35a45a611454 from https://github.com/antonblanchard/microwatt.
    > commit d3291dbdf01960e900e1d863f02f35a45a611454
    > Merge: 8bee4ae 9d7df2d
    > Author: Anton Blanchard <anton@linux.ibm.com>
    > Date:   Fri Apr 3 10:36:36 2020 +1100
    >
    >     Merge pull request #155 from mikey/exceptions
    >
    >     Add decrementer, illegal and system call exceptions
    >
    Updated using v0.0-37-ga96d504 from https://github.com/litex-hub/litex-data-auto

--- litex/data/cpu/microwatt/__init__.py
+++ litex/data/cpu/microwatt/__init__.py
@@ -2,32 +2,12 @@ import os.path
 __dir__ = os.path.split(os.path.abspath(os.path.realpath(__file__)))[0]
 data_location = os.path.join(__dir__, "vhdl")
 src = "https://github.com/antonblanchard/microwatt"
-
-# Module version
-version_str = "0.0.post474"
-version_tuple = (0, 0, 474)
+git_hash = "d3291dbdf01960e900e1d863f02f35a45a611454"
+git_describe = "v0.0-431-gd3291db"
+version_str = "0.0.post431"
+version_tuple = (0, 0)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post474")
-except ImportError:
-    pass
-
-# Data version info
-data_version_str = "0.0.post431"
-data_version_tuple = (0, 0, 431)
-try:
-    from packaging.version import Version as V
-    pdata_version = V("0.0.post431")
-except ImportError:
-    pass
-data_git_hash = "d3291dbdf01960e900e1d863f02f35a45a611454"
-data_git_describe = "v0.0-431-gd3291db"
-
-# Tool version info
-tool_version_str = "0.0.post43"
-tool_version_tuple = (0, 0, 43)
-try:
-    from packaging.version import Version as V
-    ptool_version = V("0.0.post43")
+    pversion = V("0.0.post431")
 except ImportError:
     pass
