4eea4e3ad99e4078f5fce82fd65e92d85292a2cb
LiteX <robot@timvideos.us>
2020-04-06 15:27:49 +0000

    Updating module to 0.0.post474
    Updated data to v0.0-431-gd3291db based on d3291dbdf01960e900e1d863f02f35a45a611454 from https://github.com/antonblanchard/microwatt.
    > commit d3291dbdf01960e900e1d863f02f35a45a611454
    > Merge: 8bee4ae 9d7df2d
    > Author: Anton Blanchard <anton@linux.ibm.com>
    > Date:   Fri Apr 3 10:36:36 2020 +1100
    >
    >     Merge pull request #155 from mikey/exceptions
    >
    >     Add decrementer, illegal and system call exceptions
    >
    
    Updated using 0.0.post43 from https://github.com/litex-hub/litex-data-auto

--- test.py
+++ test.py
@@ -3,8 +3,18 @@ import os
 
 from litex.data.cpu import microwatt
 
-print("Found microwatt @ version", microwatt.version_str, "("+microwatt.git_hash+")")
+print("Found microwatt @ version", microwatt.version_str, "(with data", microwatt.data_version_str, ")")
+print()
 print("Data is in", microwatt.data_location)
 assert os.path.exists(microwatt.data_location)
-print("It contains:\n -", end=" ")
-print("\n - ".join(os.listdir(microwatt.data_location)))
+print("Data is version", microwatt.data_version_str, microwatt.data_git_hash)
+print("-"*75)
+print(microwatt.data_git_msg)
+print("-"*75)
+print()
+print("It contains:")
+for root, dirs, files in os.walk(microwatt.data_location):
+    dirs.sort()
+    for f in sorted(files):
+        path = os.path.relpath(os.path.join(root, f), microwatt.data_location)
+        print(" -", path)
