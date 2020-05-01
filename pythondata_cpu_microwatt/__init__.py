36197c7b4d707b319cbe6b8cfd69808dafb3ba09
LiteX <robot@timvideos.us>
2020-05-01 17:18:31 +0000

    Updating pythondata-cpu-microwatt to 0.0.post514
    Updated data to v0.0-463-g098c3fb based on 098c3fbb2bd0a750475c91128d293b03adde5aa5 from https://github.com/antonblanchard/microwatt.
    > commit 098c3fbb2bd0a750475c91128d293b03adde5aa5
    > Merge: f21f9dd c818853
    > Author: Anton Blanchard <anton@linux.ibm.com>
    > Date:   Fri May 1 19:52:59 2020 +1000
    >
    >     Merge pull request #167 from tomtor/patch-1
    >
    >     Fix Rust README.md
    >
    
    Updated using 0.0.post51 from https://github.com/litex-hub/litex-data-auto

--- pythondata_cpu_microwatt/__init__.py
+++ pythondata_cpu_microwatt/__init__.py
@@ -4,42 +4,42 @@ data_location = os.path.join(__dir__, "vhdl")
 src = "https://github.com/antonblanchard/microwatt"
 
 # Module version
-version_str = "0.0.post511"
-version_tuple = (0, 0, 511)
+version_str = "0.0.post514"
+version_tuple = (0, 0, 514)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post511")
+    pversion = V("0.0.post514")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post461"
-data_version_tuple = (0, 0, 461)
+data_version_str = "0.0.post463"
+data_version_tuple = (0, 0, 463)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post461")
+    pdata_version = V("0.0.post463")
 except ImportError:
     pass
-data_git_hash = "f21f9dd5a0acbe9215703161f599252f1bf87d80"
-data_git_describe = "v0.0-461-gf21f9dd"
+data_git_hash = "098c3fbb2bd0a750475c91128d293b03adde5aa5"
+data_git_describe = "v0.0-463-g098c3fb"
 data_git_msg = """\
-commit f21f9dd5a0acbe9215703161f599252f1bf87d80
-Merge: b6bd1ba ff162e4
+commit 098c3fbb2bd0a750475c91128d293b03adde5aa5
+Merge: f21f9dd c818853
 Author: Anton Blanchard <anton@linux.ibm.com>
-Date:   Thu Apr 23 19:50:16 2020 +1000
+Date:   Fri May 1 19:52:59 2020 +1000
 
-    Merge pull request #164 from mikey/tags
+    Merge pull request #167 from tomtor/patch-1
     
-    Add VHDL TAGS
+    Fix Rust README.md
 
 """
 
 # Tool version info
-tool_version_str = "0.0.post50"
-tool_version_tuple = (0, 0, 50)
+tool_version_str = "0.0.post51"
+tool_version_tuple = (0, 0, 51)
 try:
     from packaging.version import Version as V
-    ptool_version = V("0.0.post50")
+    ptool_version = V("0.0.post51")
 except ImportError:
     pass
 
