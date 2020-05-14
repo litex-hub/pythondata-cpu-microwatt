d03bd5386b7a2c09fc2c985e65a966a34b07b688
LiteX <robot@timvideos.us>
2020-05-14 05:47:03 +0000

    Updating pythondata-cpu-microwatt to 0.0.post548
    Updated data to v0.0-497-gfcec66a based on fcec66acf4dc9b4b5480bf18a118a42ec87c7eba from https://github.com/antonblanchard/microwatt.
    > commit fcec66acf4dc9b4b5480bf18a118a42ec87c7eba
    > Merge: 1ba29a4 e3013f5
    > Author: Anton Blanchard <anton@linux.ibm.com>
    > Date:   Thu May 14 15:08:33 2020 +1000
    >
    >     Merge pull request #170 from antonblanchard/litedram
    >
    >     LiteDRAM integration
    >
    
    Updated using 0.0.post51 from https://github.com/litex-hub/litex-data-auto

--- pythondata_cpu_microwatt/__init__.py
+++ pythondata_cpu_microwatt/__init__.py
@@ -4,33 +4,33 @@ data_location = os.path.join(__dir__, "vhdl")
 src = "https://github.com/antonblanchard/microwatt"
 
 # Module version
-version_str = "0.0.post531"
-version_tuple = (0, 0, 531)
+version_str = "0.0.post548"
+version_tuple = (0, 0, 548)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post531")
+    pversion = V("0.0.post548")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post480"
-data_version_tuple = (0, 0, 480)
+data_version_str = "0.0.post497"
+data_version_tuple = (0, 0, 497)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post480")
+    pdata_version = V("0.0.post497")
 except ImportError:
     pass
-data_git_hash = "1ba29a407a223466ff959bd88c4a91e1f85ff622"
-data_git_describe = "v0.0-480-g1ba29a4"
+data_git_hash = "fcec66acf4dc9b4b5480bf18a118a42ec87c7eba"
+data_git_describe = "v0.0-497-gfcec66a"
 data_git_msg = """\
-commit 1ba29a407a223466ff959bd88c4a91e1f85ff622
-Merge: 4160f21 102fbcf
+commit fcec66acf4dc9b4b5480bf18a118a42ec87c7eba
+Merge: 1ba29a4 e3013f5
 Author: Anton Blanchard <anton@linux.ibm.com>
-Date:   Thu May 7 09:59:19 2020 +1000
+Date:   Thu May 14 15:08:33 2020 +1000
 
-    Merge pull request #166 from paulusmack/master
+    Merge pull request #170 from antonblanchard/litedram
     
-    MSR fixes, implement privilege checking, implement dcbz
+    LiteDRAM integration
 
 """
 
