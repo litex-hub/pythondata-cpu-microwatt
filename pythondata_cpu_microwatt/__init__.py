b7f97e87520b3da50661fb3ef6ecb14717cc5f4d
LiteX <robot@timvideos.us>
2020-05-20 05:51:26 +0000

    Updating pythondata-cpu-microwatt to 0.0.post606
    Updated data to v0.0-555-gb82d07c based on b82d07c8d569494721043d4a9d6d93a22590f335 from https://github.com/antonblanchard/microwatt.
    > commit b82d07c8d569494721043d4a9d6d93a22590f335
    > Merge: 354e0fb 8c028f2
    > Author: Anton Blanchard <anton@linux.ibm.com>
    > Date:   Tue May 19 15:53:54 2020 +1000
    >
    >     Merge pull request #179 from antonblanchard/yosys-verilator
    >
    >     Add yosys/verilator support
    >
    
    Updated using 0.0.post51 from https://github.com/litex-hub/litex-data-auto

--- pythondata_cpu_microwatt/__init__.py
+++ pythondata_cpu_microwatt/__init__.py
@@ -4,33 +4,33 @@ data_location = os.path.join(__dir__, "vhdl")
 src = "https://github.com/antonblanchard/microwatt"
 
 # Module version
-version_str = "0.0.post603"
-version_tuple = (0, 0, 603)
+version_str = "0.0.post606"
+version_tuple = (0, 0, 606)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post603")
+    pversion = V("0.0.post606")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post552"
-data_version_tuple = (0, 0, 552)
+data_version_str = "0.0.post555"
+data_version_tuple = (0, 0, 555)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post552")
+    pdata_version = V("0.0.post555")
 except ImportError:
     pass
-data_git_hash = "354e0fbfea8e0771640522e2c816e8ac13c89229"
-data_git_describe = "v0.0-552-g354e0fb"
+data_git_hash = "b82d07c8d569494721043d4a9d6d93a22590f335"
+data_git_describe = "v0.0-555-gb82d07c"
 data_git_msg = """\
-commit 354e0fbfea8e0771640522e2c816e8ac13c89229
-Merge: 6692f0d 5860c2d
+commit b82d07c8d569494721043d4a9d6d93a22590f335
+Merge: 354e0fb 8c028f2
 Author: Anton Blanchard <anton@linux.ibm.com>
-Date:   Tue May 19 14:27:42 2020 +1000
+Date:   Tue May 19 15:53:54 2020 +1000
 
-    Merge pull request #171 from shenki/mw-debug-features
+    Merge pull request #179 from antonblanchard/yosys-verilator
     
-    mw debug features
+    Add yosys/verilator support
 
 """
 
