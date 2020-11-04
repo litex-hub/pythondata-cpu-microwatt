a8098c81b16050cf5043be89aa8c690731f1c0ac
LiteX <robot@timvideos.us>
2020-11-04 22:26:50 +0000

    Updating pythondata-cpu-microwatt to 0.0.post873
    Updated data to v0.0-807-ge40e752 based on e40e752b9ab602f5ce1eb79be1fe96932558830d from https://github.com/antonblanchard/microwatt.
    > commit e40e752b9ab602f5ce1eb79be1fe96932558830d
    > Merge: 168d30c 73f8193
    > Author: Michael Neuling <mikey@neuling.org>
    > Date:   Thu Sep 17 12:04:26 2020 +1000
    >
    >     Merge pull request #245 from paulusmack/fpu
    >
    >     Add a simple FPU
    >
    
    Updated using 0.0.post66 from https://github.com/litex-hub/litex-data-auto

--- pythondata_cpu_microwatt/__init__.py
+++ pythondata_cpu_microwatt/__init__.py
@@ -4,42 +4,42 @@ data_location = os.path.join(__dir__, "vhdl")
 src = "https://github.com/antonblanchard/microwatt"
 
 # Module version
-version_str = "0.0.post755"
-version_tuple = (0, 0, 755)
+version_str = "0.0.post873"
+version_tuple = (0, 0, 873)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post755")
+    pversion = V("0.0.post873")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post695"
-data_version_tuple = (0, 0, 695)
+data_version_str = "0.0.post807"
+data_version_tuple = (0, 0, 807)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post695")
+    pdata_version = V("0.0.post807")
 except ImportError:
     pass
-data_git_hash = "ce0205b262f5731900b054dfc43f06f7af14aa98"
-data_git_describe = "v0.0-695-gce0205b"
+data_git_hash = "e40e752b9ab602f5ce1eb79be1fe96932558830d"
+data_git_describe = "v0.0-807-ge40e752"
 data_git_msg = """\
-commit ce0205b262f5731900b054dfc43f06f7af14aa98
-Merge: 419c9a6 7406219
+commit e40e752b9ab602f5ce1eb79be1fe96932558830d
+Merge: 168d30c 73f8193
 Author: Michael Neuling <mikey@neuling.org>
-Date:   Tue Jun 30 15:47:36 2020 +1000
+Date:   Thu Sep 17 12:04:26 2020 +1000
 
-    Merge pull request #216 from paulusmack/cfar
+    Merge pull request #245 from paulusmack/fpu
     
-    Timing and speed improvements, implement CFAR register
+    Add a simple FPU
 
 """
 
 # Tool version info
-tool_version_str = "0.0.post60"
-tool_version_tuple = (0, 0, 60)
+tool_version_str = "0.0.post66"
+tool_version_tuple = (0, 0, 66)
 try:
     from packaging.version import Version as V
-    ptool_version = V("0.0.post60")
+    ptool_version = V("0.0.post66")
 except ImportError:
     pass
 
