c884dc21a137f27608597d60a438308ee5a1db2d
LiteX <robot@timvideos.us>
2020-12-01 23:38:30 +0000

    Updating pythondata-cpu-microwatt to 0.0.post885
    Updated data to v0.0-819-g16da9b5 based on 16da9b5ba7702e8d34f6354b86b36c7e9bf471ea from https://github.com/antonblanchard/microwatt.
    > commit 16da9b5ba7702e8d34f6354b86b36c7e9bf471ea
    > Merge: b60026e 29fabeb
    > Author: Michael Neuling <mikey@neuling.org>
    > Date:   Tue Dec 1 11:25:08 2020 +1100
    >
    >     Merge pull request #249 from paulusmack/master
    >
    >     Sundry bug fixes, plus implement mtmsr
    >
    
    Updated using 0.0.post66 from https://github.com/litex-hub/litex-data-auto

--- pythondata_cpu_microwatt/__init__.py
+++ pythondata_cpu_microwatt/__init__.py
@@ -4,33 +4,33 @@ data_location = os.path.join(__dir__, "vhdl")
 src = "https://github.com/antonblanchard/microwatt"
 
 # Module version
-version_str = "0.0.post873"
-version_tuple = (0, 0, 873)
+version_str = "0.0.post885"
+version_tuple = (0, 0, 885)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post873")
+    pversion = V("0.0.post885")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post807"
-data_version_tuple = (0, 0, 807)
+data_version_str = "0.0.post819"
+data_version_tuple = (0, 0, 819)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post807")
+    pdata_version = V("0.0.post819")
 except ImportError:
     pass
-data_git_hash = "e40e752b9ab602f5ce1eb79be1fe96932558830d"
-data_git_describe = "v0.0-807-ge40e752"
+data_git_hash = "16da9b5ba7702e8d34f6354b86b36c7e9bf471ea"
+data_git_describe = "v0.0-819-g16da9b5"
 data_git_msg = """\
-commit e40e752b9ab602f5ce1eb79be1fe96932558830d
-Merge: 168d30c 73f8193
+commit 16da9b5ba7702e8d34f6354b86b36c7e9bf471ea
+Merge: b60026e 29fabeb
 Author: Michael Neuling <mikey@neuling.org>
-Date:   Thu Sep 17 12:04:26 2020 +1000
+Date:   Tue Dec 1 11:25:08 2020 +1100
 
-    Merge pull request #245 from paulusmack/fpu
+    Merge pull request #249 from paulusmack/master
     
-    Add a simple FPU
+    Sundry bug fixes, plus implement mtmsr
 
 """
 
