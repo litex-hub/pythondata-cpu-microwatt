edf394e28fbd61785a01d554fba4c072768f2fc4
LiteX Robot <robot@timvideos.us>
2021-02-04 06:20:27 +0000

    Updating pythondata-cpu-microwatt to 0.0.post936
    Updated data to v0.0-848-g5f8279a based on 5f8279a14ab2921df91babd684f6a4991c59ac29 from https://github.com/antonblanchard/microwatt.
    > commit 5f8279a14ab2921df91babd684f6a4991c59ac29
    > Merge: 45b7312 740f013
    > Author: Paul Mackerras <paulus@ozlabs.org>
    > Date:   Thu Jan 7 14:47:11 2021 +1100
    >
    >     Merge pull request #263 from antonblanchard/reset-pid
    >
    >     Initialize PID register
    >
    
    Updated using 0.0.post88 from https://github.com/litex-hub/litex-data-auto

--- pythondata_cpu_microwatt/__init__.py
+++ pythondata_cpu_microwatt/__init__.py
@@ -4,11 +4,11 @@ data_location = os.path.join(__dir__, "vhdl")
 src = "https://github.com/antonblanchard/microwatt"
 
 # Module version
-version_str = "0.0.post935"
-version_tuple = (0, 0, 935)
+version_str = "0.0.post936"
+version_tuple = (0, 0, 936)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post935")
+    pversion = V("0.0.post936")
 except ImportError:
     pass
 
@@ -35,11 +35,11 @@ Date:   Thu Jan 7 14:47:11 2021 +1100
 """
 
 # Tool version info
-tool_version_str = "0.0.post87"
-tool_version_tuple = (0, 0, 87)
+tool_version_str = "0.0.post88"
+tool_version_tuple = (0, 0, 88)
 try:
     from packaging.version import Version as V
-    ptool_version = V("0.0.post87")
+    ptool_version = V("0.0.post88")
 except ImportError:
     pass
 
