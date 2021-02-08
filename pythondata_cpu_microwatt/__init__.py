5b9589db6bcd7fb0f380ffdb68f8810e87a11be2
LiteX Robot <robot@timvideos.us>
2021-02-08 23:12:55 +0000

    Updating pythondata-cpu-microwatt to 0.0.post982
    Updated data to v0.0-893-g6523acc based on 6523acc74344b95e7cceb83611fb8cb2a030c1a3 from https://github.com/antonblanchard/microwatt.
    > commit 6523acc74344b95e7cceb83611fb8cb2a030c1a3
    > Merge: 6c76890 d26a157
    > Author: Michael Neuling <mikey@neuling.org>
    > Date:   Tue Feb 9 10:06:03 2021 +1100
    >
    >     Merge pull request #274 from mikey/read-sprs
    >
    >     Fix reading DSISR/DAR before writing and add a test to read from all SPRs
    >
    
    Updated using 0.0.post89 from https://github.com/litex-hub/litex-data-auto

--- pythondata_cpu_microwatt/__init__.py
+++ pythondata_cpu_microwatt/__init__.py
@@ -4,33 +4,33 @@ data_location = os.path.join(__dir__, "vhdl")
 src = "https://github.com/antonblanchard/microwatt"
 
 # Module version
-version_str = "0.0.post979"
-version_tuple = (0, 0, 979)
+version_str = "0.0.post982"
+version_tuple = (0, 0, 982)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post979")
+    pversion = V("0.0.post982")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post890"
-data_version_tuple = (0, 0, 890)
+data_version_str = "0.0.post893"
+data_version_tuple = (0, 0, 893)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post890")
+    pdata_version = V("0.0.post893")
 except ImportError:
     pass
-data_git_hash = "6c7689052dbd76b5fe7c9f4804d6a4817e9845af"
-data_git_describe = "v0.0-890-g6c76890"
+data_git_hash = "6523acc74344b95e7cceb83611fb8cb2a030c1a3"
+data_git_describe = "v0.0-893-g6523acc"
 data_git_msg = """\
-commit 6c7689052dbd76b5fe7c9f4804d6a4817e9845af
-Merge: 9a6a7e9 17fd069
+commit 6523acc74344b95e7cceb83611fb8cb2a030c1a3
+Merge: 6c76890 d26a157
 Author: Michael Neuling <mikey@neuling.org>
-Date:   Mon Feb 8 17:27:16 2021 +1100
+Date:   Tue Feb 9 10:06:03 2021 +1100
 
-    Merge pull request #269 from paulusmack/pipeline
+    Merge pull request #274 from mikey/read-sprs
     
-    Rework load/store pipeline to achieve one load/store per cycle throughput
+    Fix reading DSISR/DAR before writing and add a test to read from all SPRs
 
 """
 
