b3ba213ac12875c5faa03bc6b0f75145b8eb1d0b
LiteX Robot <robot@timvideos.us>
2021-03-05 19:53:12 +0000

    Updating pythondata-cpu-microwatt to 0.0.post985
    Updated data to v0.0-893-g6523acc based on 6523acc74344b95e7cceb83611fb8cb2a030c1a3 from https://github.com/antonblanchard/microwatt.
    > commit 6523acc74344b95e7cceb83611fb8cb2a030c1a3
    > Merge: 6c76890 d26a157
    > Author: Michael Neuling <mikey@neuling.org>
    > Date:   Tue Feb 9 10:06:03 2021 +1100
    >
    >     Merge pull request #274 from mikey/read-sprs
    >
    >     Fix reading DSISR/DAR before writing and add a test to read from all SPRs
    >
    
    Updated using 0.0.post92 from https://github.com/litex-hub/litex-data-auto

--- pythondata_cpu_microwatt/__init__.py
+++ pythondata_cpu_microwatt/__init__.py
@@ -4,11 +4,11 @@ data_location = os.path.join(__dir__, "vhdl")
 src = "https://github.com/antonblanchard/microwatt"
 
 # Module version
-version_str = "0.0.post984"
-version_tuple = (0, 0, 984)
+version_str = "0.0.post985"
+version_tuple = (0, 0, 985)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post984")
+    pversion = V("0.0.post985")
 except ImportError:
     pass
 
@@ -35,11 +35,11 @@ Date:   Tue Feb 9 10:06:03 2021 +1100
 """
 
 # Tool version info
-tool_version_str = "0.0.post91"
-tool_version_tuple = (0, 0, 91)
+tool_version_str = "0.0.post92"
+tool_version_tuple = (0, 0, 92)
 try:
     from packaging.version import Version as V
-    ptool_version = V("0.0.post91")
+    ptool_version = V("0.0.post92")
 except ImportError:
     pass
 
