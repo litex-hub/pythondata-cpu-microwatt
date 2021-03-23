5283701594a3f0c52cd2ea45bf99bbd13b8132f9
LiteX Robot <robot@timvideos.us>
2021-03-23 09:14:51 +0000

    Updating pythondata-cpu-microwatt to 0.0.post990
    Updated data to v0.0-895-gef01fa3 based on ef01fa32bd888215ffc688389296f17676667590 from https://github.com/antonblanchard/microwatt.
    > commit ef01fa32bd888215ffc688389296f17676667590
    > Merge: 6523acc 91a53d8
    > Author: Anton Blanchard <anton@linux.ibm.com>
    > Date:   Tue Mar 23 20:02:27 2021 +1100
    >
    >     Merge pull request #284 from antonblanchard/boot-clocks
    >
    >     Allow SPI BOOT_CLOCKS to be overridden by top level
    >
    
    Updated using 0.0.post95 from https://github.com/litex-hub/litex-data-auto

--- pythondata_cpu_microwatt/__init__.py
+++ pythondata_cpu_microwatt/__init__.py
@@ -4,33 +4,33 @@ data_location = os.path.join(__dir__, "vhdl")
 src = "https://github.com/antonblanchard/microwatt"
 
 # Module version
-version_str = "0.0.post988"
-version_tuple = (0, 0, 988)
+version_str = "0.0.post990"
+version_tuple = (0, 0, 990)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post988")
+    pversion = V("0.0.post990")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post893"
-data_version_tuple = (0, 0, 893)
+data_version_str = "0.0.post895"
+data_version_tuple = (0, 0, 895)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post893")
+    pdata_version = V("0.0.post895")
 except ImportError:
     pass
-data_git_hash = "6523acc74344b95e7cceb83611fb8cb2a030c1a3"
-data_git_describe = "v0.0-893-g6523acc"
+data_git_hash = "ef01fa32bd888215ffc688389296f17676667590"
+data_git_describe = "v0.0-895-gef01fa3"
 data_git_msg = """\
-commit 6523acc74344b95e7cceb83611fb8cb2a030c1a3
-Merge: 6c76890 d26a157
-Author: Michael Neuling <mikey@neuling.org>
-Date:   Tue Feb 9 10:06:03 2021 +1100
+commit ef01fa32bd888215ffc688389296f17676667590
+Merge: 6523acc 91a53d8
+Author: Anton Blanchard <anton@linux.ibm.com>
+Date:   Tue Mar 23 20:02:27 2021 +1100
 
-    Merge pull request #274 from mikey/read-sprs
+    Merge pull request #284 from antonblanchard/boot-clocks
     
-    Fix reading DSISR/DAR before writing and add a test to read from all SPRs
+    Allow SPI BOOT_CLOCKS to be overridden by top level
 
 """
 
