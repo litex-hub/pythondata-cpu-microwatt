6a426b1acc6ab3834ba2f248f04bc0ea4e765250
LiteX Robot <robot@timvideos.us>
2021-03-24 10:05:26 +0000

    Updating pythondata-cpu-microwatt to 0.0.post992
    Updated data to v0.0-897-g5cc5d8f based on 5cc5d8f030d303a82ed38f3b359921a748b746ba from https://github.com/antonblanchard/microwatt.
    > commit 5cc5d8f030d303a82ed38f3b359921a748b746ba
    > Merge: ef01fa3 2d21b95
    > Author: Anton Blanchard <anton@linux.ibm.com>
    > Date:   Wed Mar 24 20:44:45 2021 +1100
    >
    >     Merge pull request #281 from antonblanchard/cache-tlb-parameters
    >
    >     Pass icache/dcache/tlb parameters down from soc
    >
    
    Updated using 0.0.post95 from https://github.com/litex-hub/litex-data-auto

--- pythondata_cpu_microwatt/__init__.py
+++ pythondata_cpu_microwatt/__init__.py
@@ -4,33 +4,33 @@ data_location = os.path.join(__dir__, "vhdl")
 src = "https://github.com/antonblanchard/microwatt"
 
 # Module version
-version_str = "0.0.post990"
-version_tuple = (0, 0, 990)
+version_str = "0.0.post992"
+version_tuple = (0, 0, 992)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post990")
+    pversion = V("0.0.post992")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post895"
-data_version_tuple = (0, 0, 895)
+data_version_str = "0.0.post897"
+data_version_tuple = (0, 0, 897)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post895")
+    pdata_version = V("0.0.post897")
 except ImportError:
     pass
-data_git_hash = "ef01fa32bd888215ffc688389296f17676667590"
-data_git_describe = "v0.0-895-gef01fa3"
+data_git_hash = "5cc5d8f030d303a82ed38f3b359921a748b746ba"
+data_git_describe = "v0.0-897-g5cc5d8f"
 data_git_msg = """\
-commit ef01fa32bd888215ffc688389296f17676667590
-Merge: 6523acc 91a53d8
+commit 5cc5d8f030d303a82ed38f3b359921a748b746ba
+Merge: ef01fa3 2d21b95
 Author: Anton Blanchard <anton@linux.ibm.com>
-Date:   Tue Mar 23 20:02:27 2021 +1100
+Date:   Wed Mar 24 20:44:45 2021 +1100
 
-    Merge pull request #284 from antonblanchard/boot-clocks
+    Merge pull request #281 from antonblanchard/cache-tlb-parameters
     
-    Allow SPI BOOT_CLOCKS to be overridden by top level
+    Pass icache/dcache/tlb parameters down from soc
 
 """
 
