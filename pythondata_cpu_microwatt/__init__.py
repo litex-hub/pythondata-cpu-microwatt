fc79df7285cffdf89e24c07a3455da743c8747b7
LiteX Robot <robot@timvideos.us>
2021-03-24 11:05:11 +0000

    Updating pythondata-cpu-microwatt to 0.0.post996
    Updated data to v0.0-901-g0af9062 based on 0af906232f51827d624f570c1dc8eaeee8713725 from https://github.com/antonblanchard/microwatt.
    > commit 0af906232f51827d624f570c1dc8eaeee8713725
    > Merge: 5cc5d8f 4ab3651
    > Author: Anton Blanchard <anton@linux.ibm.com>
    > Date:   Wed Mar 24 21:31:54 2021 +1100
    >
    >     Merge pull request #285 from antonblanchard/Makefile-cleanup-2
    >
    >     A few Makefile cleanups
    >
    
    Updated using 0.0.post95 from https://github.com/litex-hub/litex-data-auto

--- pythondata_cpu_microwatt/__init__.py
+++ pythondata_cpu_microwatt/__init__.py
@@ -4,33 +4,33 @@ data_location = os.path.join(__dir__, "vhdl")
 src = "https://github.com/antonblanchard/microwatt"
 
 # Module version
-version_str = "0.0.post992"
-version_tuple = (0, 0, 992)
+version_str = "0.0.post996"
+version_tuple = (0, 0, 996)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post992")
+    pversion = V("0.0.post996")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post897"
-data_version_tuple = (0, 0, 897)
+data_version_str = "0.0.post901"
+data_version_tuple = (0, 0, 901)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post897")
+    pdata_version = V("0.0.post901")
 except ImportError:
     pass
-data_git_hash = "5cc5d8f030d303a82ed38f3b359921a748b746ba"
-data_git_describe = "v0.0-897-g5cc5d8f"
+data_git_hash = "0af906232f51827d624f570c1dc8eaeee8713725"
+data_git_describe = "v0.0-901-g0af9062"
 data_git_msg = """\
-commit 5cc5d8f030d303a82ed38f3b359921a748b746ba
-Merge: ef01fa3 2d21b95
+commit 0af906232f51827d624f570c1dc8eaeee8713725
+Merge: 5cc5d8f 4ab3651
 Author: Anton Blanchard <anton@linux.ibm.com>
-Date:   Wed Mar 24 20:44:45 2021 +1100
+Date:   Wed Mar 24 21:31:54 2021 +1100
 
-    Merge pull request #281 from antonblanchard/cache-tlb-parameters
+    Merge pull request #285 from antonblanchard/Makefile-cleanup-2
     
-    Pass icache/dcache/tlb parameters down from soc
+    A few Makefile cleanups
 
 """
 
