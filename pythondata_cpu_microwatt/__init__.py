82fc19997ec56572d261dae564c4b0d1394685eb
LiteX Robot <robot@timvideos.us>
2021-03-25 04:06:42 +0000

    Updating pythondata-cpu-microwatt to 0.0.post999
    Updated data to v0.0-904-g6d827b9 based on 6d827b93580684e5977f5d7340ed128521161f42 from https://github.com/antonblanchard/microwatt.
    > commit 6d827b93580684e5977f5d7340ed128521161f42
    > Merge: 0af9062 be11ebb
    > Author: Anton Blanchard <anton@linux.ibm.com>
    > Date:   Thu Mar 25 14:43:45 2021 +1100
    >
    >     Merge pull request #286 from antonblanchard/Makefile-cleanup-3
    >
    >     A few more Makefile cleanups
    >
    
    Updated using 0.0.post95 from https://github.com/litex-hub/litex-data-auto

--- pythondata_cpu_microwatt/__init__.py
+++ pythondata_cpu_microwatt/__init__.py
@@ -4,33 +4,33 @@ data_location = os.path.join(__dir__, "vhdl")
 src = "https://github.com/antonblanchard/microwatt"
 
 # Module version
-version_str = "0.0.post996"
-version_tuple = (0, 0, 996)
+version_str = "0.0.post999"
+version_tuple = (0, 0, 999)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post996")
+    pversion = V("0.0.post999")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post901"
-data_version_tuple = (0, 0, 901)
+data_version_str = "0.0.post904"
+data_version_tuple = (0, 0, 904)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post901")
+    pdata_version = V("0.0.post904")
 except ImportError:
     pass
-data_git_hash = "0af906232f51827d624f570c1dc8eaeee8713725"
-data_git_describe = "v0.0-901-g0af9062"
+data_git_hash = "6d827b93580684e5977f5d7340ed128521161f42"
+data_git_describe = "v0.0-904-g6d827b9"
 data_git_msg = """\
-commit 0af906232f51827d624f570c1dc8eaeee8713725
-Merge: 5cc5d8f 4ab3651
+commit 6d827b93580684e5977f5d7340ed128521161f42
+Merge: 0af9062 be11ebb
 Author: Anton Blanchard <anton@linux.ibm.com>
-Date:   Wed Mar 24 21:31:54 2021 +1100
+Date:   Thu Mar 25 14:43:45 2021 +1100
 
-    Merge pull request #285 from antonblanchard/Makefile-cleanup-2
+    Merge pull request #286 from antonblanchard/Makefile-cleanup-3
     
-    A few Makefile cleanups
+    A few more Makefile cleanups
 
 """
 
