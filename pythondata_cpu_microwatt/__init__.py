5799aadc241c5afc4b36e819f15ebea1aa3023aa
LiteX Robot <robot@timvideos.us>
2022-01-13 17:08:15 +0000

    Updating pythondata-cpu-microwatt to 0.0.post1162
    Updated data to v0.0-1042-g67164a6 based on 67164a6ffa41672d8779f62efb0a5a7d96b7a1f7 from https://github.com/antonblanchard/microwatt.
    > commit 67164a6ffa41672d8779f62efb0a5a7d96b7a1f7
    > Merge: 7fa7b45 9ceb463
    > Author: Anton Blanchard <anton@linux.ibm.com>
    > Date:   Sun Jan 9 08:08:48 2022 +1100
    >
    >     Merge pull request #338 from shenki/yosys-read-verilog
    >
    >     Makefile: Use read_verilog with yosys
    >
    
    Updated using 0.0.post120 from https://github.com/litex-hub/litex-data-auto

--- pythondata_cpu_microwatt/__init__.py
+++ pythondata_cpu_microwatt/__init__.py
@@ -4,11 +4,11 @@ data_location = os.path.join(__dir__, "vhdl")
 src = "https://github.com/antonblanchard/microwatt"
 
 # Module version
-version_str = "0.0.post1160"
-version_tuple = (0, 0, 1160)
+version_str = "0.0.post1162"
+version_tuple = (0, 0, 1162)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post1160")
+    pversion = V("0.0.post1162")
 except ImportError:
     pass
 
@@ -35,11 +35,11 @@ Date:   Sun Jan 9 08:08:48 2022 +1100
 """
 
 # Tool version info
-tool_version_str = "0.0.post118"
-tool_version_tuple = (0, 0, 118)
+tool_version_str = "0.0.post120"
+tool_version_tuple = (0, 0, 120)
 try:
     from packaging.version import Version as V
-    ptool_version = V("0.0.post118")
+    ptool_version = V("0.0.post120")
 except ImportError:
     pass
 
