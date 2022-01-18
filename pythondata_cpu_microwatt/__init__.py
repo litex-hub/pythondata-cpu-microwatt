280c4b70bdf739603096907672ebdbde741d3afb
LiteX Robot <robot@timvideos.us>
2022-01-18 01:25:20 +0000

    Updating pythondata-cpu-microwatt to 0.0.post1169
    Updated data to v0.0-1047-gfda8879 based on fda8879e2f721e4a01f8f9e946ac9cf6d1d1a363 from https://github.com/antonblanchard/microwatt.
    > commit fda8879e2f721e4a01f8f9e946ac9cf6d1d1a363
    > Merge: ffbf2f9 5e90133
    > Author: Michael Neuling <mikey@neuling.org>
    > Date:   Tue Jan 18 11:51:54 2022 +1100
    >
    >     Merge pull request #341 from mkj/progtools
    >
    >     orangecrab programming targets
    >
    
    Updated using 0.0.post122 from https://github.com/litex-hub/litex-data-auto

--- pythondata_cpu_microwatt/__init__.py
+++ pythondata_cpu_microwatt/__init__.py
@@ -4,33 +4,33 @@ data_location = os.path.join(__dir__, "vhdl")
 src = "https://github.com/antonblanchard/microwatt"
 
 # Module version
-version_str = "0.0.post1164"
-version_tuple = (0, 0, 1164)
+version_str = "0.0.post1169"
+version_tuple = (0, 0, 1169)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post1164")
+    pversion = V("0.0.post1169")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post1042"
-data_version_tuple = (0, 0, 1042)
+data_version_str = "0.0.post1047"
+data_version_tuple = (0, 0, 1047)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post1042")
+    pdata_version = V("0.0.post1047")
 except ImportError:
     pass
-data_git_hash = "67164a6ffa41672d8779f62efb0a5a7d96b7a1f7"
-data_git_describe = "v0.0-1042-g67164a6"
+data_git_hash = "fda8879e2f721e4a01f8f9e946ac9cf6d1d1a363"
+data_git_describe = "v0.0-1047-gfda8879"
 data_git_msg = """\
-commit 67164a6ffa41672d8779f62efb0a5a7d96b7a1f7
-Merge: 7fa7b45 9ceb463
-Author: Anton Blanchard <anton@linux.ibm.com>
-Date:   Sun Jan 9 08:08:48 2022 +1100
+commit fda8879e2f721e4a01f8f9e946ac9cf6d1d1a363
+Merge: ffbf2f9 5e90133
+Author: Michael Neuling <mikey@neuling.org>
+Date:   Tue Jan 18 11:51:54 2022 +1100
 
-    Merge pull request #338 from shenki/yosys-read-verilog
+    Merge pull request #341 from mkj/progtools
     
-    Makefile: Use read_verilog with yosys
+    orangecrab programming targets
 
 """
 
