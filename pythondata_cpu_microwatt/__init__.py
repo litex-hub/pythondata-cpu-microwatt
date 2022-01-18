37c1147d481cbc4ad6da3a2cc57ac871cc21bcab
LiteX Robot <robot@timvideos.us>
2022-01-18 02:48:11 +0000

    Updating pythondata-cpu-microwatt to 0.0.post1181
    Updated data to v0.0-1059-g6ff3b24 based on 6ff3b2499ceaf911252f634d44469fefb58952ae from https://github.com/antonblanchard/microwatt.
    > commit 6ff3b2499ceaf911252f634d44469fefb58952ae
    > Merge: fda8879 cdd661d
    > Author: Michael Neuling <mikey@neuling.org>
    > Date:   Tue Jan 18 13:27:27 2022 +1100
    >
    >     Merge pull request #342 from mkj/orangecrab-merge
    >
    >     Orangecrab working with litedram
    >
    >     Fixed up a few simple merge conflicts in the Makefile.
    >
    
    Updated using 0.0.post122 from https://github.com/litex-hub/litex-data-auto

--- pythondata_cpu_microwatt/__init__.py
+++ pythondata_cpu_microwatt/__init__.py
@@ -4,33 +4,35 @@ data_location = os.path.join(__dir__, "vhdl")
 src = "https://github.com/antonblanchard/microwatt"
 
 # Module version
-version_str = "0.0.post1169"
-version_tuple = (0, 0, 1169)
+version_str = "0.0.post1181"
+version_tuple = (0, 0, 1181)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post1169")
+    pversion = V("0.0.post1181")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post1047"
-data_version_tuple = (0, 0, 1047)
+data_version_str = "0.0.post1059"
+data_version_tuple = (0, 0, 1059)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post1047")
+    pdata_version = V("0.0.post1059")
 except ImportError:
     pass
-data_git_hash = "fda8879e2f721e4a01f8f9e946ac9cf6d1d1a363"
-data_git_describe = "v0.0-1047-gfda8879"
+data_git_hash = "6ff3b2499ceaf911252f634d44469fefb58952ae"
+data_git_describe = "v0.0-1059-g6ff3b24"
 data_git_msg = """\
-commit fda8879e2f721e4a01f8f9e946ac9cf6d1d1a363
-Merge: ffbf2f9 5e90133
+commit 6ff3b2499ceaf911252f634d44469fefb58952ae
+Merge: fda8879 cdd661d
 Author: Michael Neuling <mikey@neuling.org>
-Date:   Tue Jan 18 11:51:54 2022 +1100
+Date:   Tue Jan 18 13:27:27 2022 +1100
 
-    Merge pull request #341 from mkj/progtools
+    Merge pull request #342 from mkj/orangecrab-merge
     
-    orangecrab programming targets
+    Orangecrab working with litedram
+    
+    Fixed up a few simple merge conflicts in the Makefile.
 
 """
 
