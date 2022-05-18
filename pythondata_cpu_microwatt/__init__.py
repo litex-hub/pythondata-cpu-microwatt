27fd6441e3a68b58a1b54b9ec57da683bff8cfd1
LiteX Robot <robot@timvideos.us>
2022-05-18 18:15:48 +0000

    Updating pythondata-cpu-microwatt to 0.0.post1219
    Updated data to v0.0-1091-gf5e06c2 based on f5e06c2d4bd8faa90a2d3c47ca7ba22343e3aae8 from https://github.com/antonblanchard/microwatt.
    > commit f5e06c2d4bd8faa90a2d3c47ca7ba22343e3aae8
    > Merge: 8bf48ac 948f6f4
    > Author: Michael Neuling <mikey@neuling.org>
    > Date:   Tue Mar 22 11:55:54 2022 +1100
    >
    >     Merge pull request #361 from antonblanchard/alt-reset-address
    >
    >     Allow ALT_RESET_ADDRESS to be overridden
    >
    
    Updated using 0.0.post128 from https://github.com/litex-hub/litex-data-auto

--- pythondata_cpu_microwatt/__init__.py
+++ pythondata_cpu_microwatt/__init__.py
@@ -4,11 +4,11 @@ data_location = os.path.join(__dir__, "vhdl")
 src = "https://github.com/antonblanchard/microwatt"
 
 # Module version
-version_str = "0.0.post1217"
-version_tuple = (0, 0, 1217)
+version_str = "0.0.post1219"
+version_tuple = (0, 0, 1219)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post1217")
+    pversion = V("0.0.post1219")
 except ImportError:
     pass
 
@@ -35,11 +35,11 @@ Date:   Tue Mar 22 11:55:54 2022 +1100
 """
 
 # Tool version info
-tool_version_str = "0.0.post126"
-tool_version_tuple = (0, 0, 126)
+tool_version_str = "0.0.post128"
+tool_version_tuple = (0, 0, 128)
 try:
     from packaging.version import Version as V
-    ptool_version = V("0.0.post126")
+    ptool_version = V("0.0.post128")
 except ImportError:
     pass
 
