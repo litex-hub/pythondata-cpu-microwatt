49b73c9d7d2942ab6660c1e3ede6970ddbb451e3
LiteX Robot <robot@timvideos.us>
2022-06-12 22:12:17 +0000

    Updating pythondata-cpu-microwatt to 0.0.post1242
    Updated data to v0.0-1100-gd3aff67 based on d3aff67fa77b5d24aa961c5e43a550f8a4d95a58 from https://github.com/antonblanchard/microwatt.
    > commit d3aff67fa77b5d24aa961c5e43a550f8a4d95a58
    > Merge: 1ff852b 71d4b5e
    > Author: Anton Blanchard <anton@linux.ibm.com>
    > Date:   Mon Jun 13 07:15:55 2022 +1000
    >
    >     Merge pull request #375 from antonblanchard/core_debug-init
    >
    >     core_debug: Initialise gspr_index
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_cpu_microwatt/__init__.py
+++ pythondata_cpu_microwatt/__init__.py
@@ -4,33 +4,33 @@ data_location = os.path.join(__dir__, "vhdl")
 src = "https://github.com/antonblanchard/microwatt"
 
 # Module version
-version_str = "0.0.post1240"
-version_tuple = (0, 0, 1240)
+version_str = "0.0.post1242"
+version_tuple = (0, 0, 1242)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post1240")
+    pversion = V("0.0.post1242")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post1098"
-data_version_tuple = (0, 0, 1098)
+data_version_str = "0.0.post1100"
+data_version_tuple = (0, 0, 1100)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post1098")
+    pdata_version = V("0.0.post1100")
 except ImportError:
     pass
-data_git_hash = "1ff852b012155ad610e9d044cac3461adc198f85"
-data_git_describe = "v0.0-1098-g1ff852b"
+data_git_hash = "d3aff67fa77b5d24aa961c5e43a550f8a4d95a58"
+data_git_describe = "v0.0-1100-gd3aff67"
 data_git_msg = """\
-commit 1ff852b012155ad610e9d044cac3461adc198f85
-Merge: b7c4d3c e243807
+commit d3aff67fa77b5d24aa961c5e43a550f8a4d95a58
+Merge: 1ff852b 71d4b5e
 Author: Anton Blanchard <anton@linux.ibm.com>
-Date:   Sun Jun 12 10:24:54 2022 +1000
+Date:   Mon Jun 13 07:15:55 2022 +1000
 
-    Merge pull request #369 from antonblanchard/loadstore-pmu-init
+    Merge pull request #375 from antonblanchard/core_debug-init
     
-    loadstore1: Initialise PMU events
+    core_debug: Initialise gspr_index
 
 """
 
