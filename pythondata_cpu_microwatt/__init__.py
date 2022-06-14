5302d09a6a392385b67f681c48099e0d40b5fe00
LiteX Robot <robot@timvideos.us>
2022-06-14 03:10:26 +0000

    Updating pythondata-cpu-microwatt to 0.0.post1244
    Updated data to v0.0-1102-gb82eea5 based on b82eea593341c3a8da0f0afbd4e8aabdc852ef6d from https://github.com/antonblanchard/microwatt.
    > commit b82eea593341c3a8da0f0afbd4e8aabdc852ef6d
    > Merge: d3aff67 ff442d1
    > Author: Michael Neuling <mikey@neuling.org>
    > Date:   Tue Jun 14 13:09:57 2022 +1000
    >
    >     Merge pull request #366 from antonblanchard/hello-world-bss
    >
    >     Zero BSS in hello world test
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_cpu_microwatt/__init__.py
+++ pythondata_cpu_microwatt/__init__.py
@@ -4,33 +4,33 @@ data_location = os.path.join(__dir__, "vhdl")
 src = "https://github.com/antonblanchard/microwatt"
 
 # Module version
-version_str = "0.0.post1242"
-version_tuple = (0, 0, 1242)
+version_str = "0.0.post1244"
+version_tuple = (0, 0, 1244)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post1242")
+    pversion = V("0.0.post1244")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post1100"
-data_version_tuple = (0, 0, 1100)
+data_version_str = "0.0.post1102"
+data_version_tuple = (0, 0, 1102)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post1100")
+    pdata_version = V("0.0.post1102")
 except ImportError:
     pass
-data_git_hash = "d3aff67fa77b5d24aa961c5e43a550f8a4d95a58"
-data_git_describe = "v0.0-1100-gd3aff67"
+data_git_hash = "b82eea593341c3a8da0f0afbd4e8aabdc852ef6d"
+data_git_describe = "v0.0-1102-gb82eea5"
 data_git_msg = """\
-commit d3aff67fa77b5d24aa961c5e43a550f8a4d95a58
-Merge: 1ff852b 71d4b5e
-Author: Anton Blanchard <anton@linux.ibm.com>
-Date:   Mon Jun 13 07:15:55 2022 +1000
+commit b82eea593341c3a8da0f0afbd4e8aabdc852ef6d
+Merge: d3aff67 ff442d1
+Author: Michael Neuling <mikey@neuling.org>
+Date:   Tue Jun 14 13:09:57 2022 +1000
 
-    Merge pull request #375 from antonblanchard/core_debug-init
+    Merge pull request #366 from antonblanchard/hello-world-bss
     
-    core_debug: Initialise gspr_index
+    Zero BSS in hello world test
 
 """
 
