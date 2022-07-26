5c2858c31c27ee470cfdc981d9c25f7e8236d9a4
LiteX Robot <robot@timvideos.us>
2022-07-26 00:48:05 +0000

    Updating pythondata-cpu-microwatt to 0.0.post1299
    Updated data to v0.0-1157-g281a125 based on 281a125f1fa9edb464dd580874899d2732a92d0f from https://github.com/antonblanchard/microwatt.
    > commit 281a125f1fa9edb464dd580874899d2732a92d0f
    > Merge: bad9a9a d6121cd
    > Author: Michael Neuling <mikey@neuling.org>
    > Date:   Tue Jul 26 10:08:33 2022 +1000
    >
    >     Merge pull request #379 from paulusmack/master
    >
    >     Lots of improvements
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_cpu_microwatt/__init__.py
+++ pythondata_cpu_microwatt/__init__.py
@@ -4,33 +4,33 @@ data_location = os.path.join(__dir__, "vhdl")
 src = "https://github.com/antonblanchard/microwatt"
 
 # Module version
-version_str = "0.0.post1268"
-version_tuple = (0, 0, 1268)
+version_str = "0.0.post1299"
+version_tuple = (0, 0, 1299)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post1268")
+    pversion = V("0.0.post1299")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post1126"
-data_version_tuple = (0, 0, 1126)
+data_version_str = "0.0.post1157"
+data_version_tuple = (0, 0, 1157)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post1126")
+    pdata_version = V("0.0.post1157")
 except ImportError:
     pass
-data_git_hash = "bad9a9a2e815c252ec984dd0b747f1dc888c863c"
-data_git_describe = "v0.0-1126-gbad9a9a"
+data_git_hash = "281a125f1fa9edb464dd580874899d2732a92d0f"
+data_git_describe = "v0.0-1157-g281a125"
 data_git_msg = """\
-commit bad9a9a2e815c252ec984dd0b747f1dc888c863c
-Merge: 35e0dbe a060ad5
+commit 281a125f1fa9edb464dd580874899d2732a92d0f
+Merge: bad9a9a d6121cd
 Author: Michael Neuling <mikey@neuling.org>
-Date:   Mon Jul 25 16:31:12 2022 +1000
+Date:   Tue Jul 26 10:08:33 2022 +1000
 
-    Merge pull request #380 from iagocaran/master
+    Merge pull request #379 from paulusmack/master
     
-    tests/pmu: Add load/store completed and instruction/cycle count
+    Lots of improvements
 
 """
 
