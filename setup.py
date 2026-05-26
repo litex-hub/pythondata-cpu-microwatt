32c2d9b6bba1c1ecf299a7914073ec8976e6177e
Florent Kermarrec <florent@enjoy-digital.fr>
2026-05-26 16:49:34 +0200

    packaging: fix release installs

--- setup.py
+++ setup.py
@@ -1,9 +1,15 @@
 import setuptools
+import re
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-from pythondata_cpu_microwatt import version_str
+with open("pythondata_cpu_microwatt/__init__.py", "r") as fh:
+    version_str = re.search(
+        r'^version_str = "([^"]+)"',
+        fh.read(),
+        re.MULTILINE,
+    ).group(1)
 
 setuptools.setup(
     name="pythondata-cpu-microwatt",
@@ -24,7 +30,7 @@ Python module containing vhdl files for microwatt cpu.""",
     zip_safe=False,
     packages=setuptools.find_packages(),
     package_data={
-    	'cpu_microwatt': ['cpu_microwatt/vhdl/**'],
+    	'pythondata_cpu_microwatt': ['vhdl/**'],
     },
     include_package_data=True,
     project_urls={
